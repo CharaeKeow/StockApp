2a91e61afee69c0b9908fdc5948c5e85e5f740c8
CharaeKeow <charaekeow11@gmail.com>
2020-11-22 01:54:03 +0800

    add flatlist in portfolio screen

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,14 +1,73 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { FlatList, TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack'
 
+const DATA = [
+  {
+    id: '00001',
+    title: 'APPL',
+    price: 'USD XXXX'
+  },
+  {
+    id: '00002',
+    title: 'AMZN',
+    price: 'USD XXXX'
+  },
+  {
+    id: '00003',
+    title: 'TSLA',
+    price: 'USD XXXX'
+  },
+  {
+    id: '00004',
+    title: 'GOOG',
+    price: 'USD XXXX'
+  }
+];
+
+const Item = ({ item, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text style={styles.title}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
 function Portfolio() {
+  const [selectedId, setSelectedId] = React.useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        style={{ backgroundColor }}
+      />
+    )
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Portfolio</Text>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 20,
+    height: 100,
+    width: 300,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+  }
+})
 
 const PortfolioStack = createStackNavigator();
 
