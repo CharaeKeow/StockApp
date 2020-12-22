3021e667b4bdf84736adb75e9cdbe6f92543302a
Skyman1208 <eye2882@gmail.com>
2020-12-23 00:02:42 +0800

    SKYMAN 23122020

--- src/styles/News.style.js
+++ src/styles/News.style.js
@@ -3,7 +3,7 @@ import { StyleSheet, Dimensions } from 'react-native';
 export default StyleSheet.create({
   item: {
     padding: 18,
-    height: 120,
+    height: 200,
     width: Dimensions.get('window').width - 25,
     marginVertical: 6,
     borderRadius: 15,
